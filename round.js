59a9ea986feffd7dbeaf9436e71e52a08eb0bc10
alanqq0624 <alanqq0624@hotmail.com>
2018-03-28 16:52:00 +0800

    JS version sun rotate 	main animate complete

--- round.js
+++ round.js
@@ -1,21 +1,37 @@
+$("#sun2").css("background-color", "blue")
+$("#sun").css("transform-origin", "50vw 0px");
+$("#sun").css("transform", " translateX(-25px)");
+$("#sun").css("transform", " translateY(-25px)");
+
+
 $("#start").click(function(){
     if($("#start").text() == "start"){
         $("#start").text("stop");
 
         //reset position and move
+        $("#sun").animate({ borderSpacing: 180},{
+            step: function(now,fx) {
+                $("p").text("now = " + now);
+                $(this).css('transform','rotate('+now+'deg)');
+            },
+            duration:'slow'
+        }, 'linear');
+
     }
     else if($("#start").text() == "stop"){
         $("#start").text("start");
 
         //let move stop
+        $("#sun").animate({ borderSpacing: 0},{
+            step: function(now,fx) {
+                $("p").text("now = " + now);
+                $(this).css('transform','rotate('+now+'deg)');
+            },
+            duration:'slow'
+        }, 'linear');
+
     }
     else{
         $("#start").text("7122");
     }
 });
-
-var xSun, ySun;
-
-$("#sun").animate(
-    
-)
